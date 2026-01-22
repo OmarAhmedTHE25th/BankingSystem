60c96622b7a61d20397e785309c6aeecb3259f6d
Omar Ahmed Abdel Hameed <omar.ahmed.nes6@gmail.com>
2026-01-22 17:32:23 +0200

    refactor main: replace hardcoded database connection with environment variable retrieval

--- main.cpp
+++ main.cpp
@@ -1,18 +1,41 @@
 #include <iostream>
 #include "userController.hpp"
 #include <pqxx/pqxx>
+#include <cstdlib>
+auto getEnv = [](const char* key) -> std::string {
+    const char* val = std::getenv(key);
+    if (!val) {
+        throw std::runtime_error(std::string("Missing env var: ") + key);
+    }
+    return {val};
+};
+
 int main() {
-    pqxx::connection Conn(
-           "host=ep-gentle-frost-ah8aaayp-pooler.c-3.us-east-1.aws.neon.tech "
-           "port=5432 "
-           "dbname=neondb "
-           "user=neondb_owner "
-           "password=npg_teqvF1lVGu0d "
-           "sslmode=require"
-       );
-    UserController uc(Conn);
-    uc.updateUser(1,std::nullopt,"my.cool.email@gmail.com");
-    uc.updateUser(45,2,"my_other.cool.email@yahoo.com");
-    uc.viewUsers();
+    try {
+        const std::string host  = getEnv("DB_HOST");
+        const std::string port  = getEnv("DB_PORT");
+        const std::string db    = getEnv("DB_NAME");
+        const std::string user  = getEnv("DB_USER");
+        const std::string pass  = getEnv("DB_PASS");
+
+        pqxx::connection Conn(
+            "host=" + host +
+            " port=" + port +
+            " dbname=" + db +
+            " user=" + user +
+            " password=" + pass +
+            " sslmode=require"
+        );
+        const UserController uc(Conn);
+        uc.updateUser(1,std::nullopt,"my.cool.email@gmail.com");
+        uc.updateUser(45,2,"my_other.cool.email@yahoo.com");
+        uc.viewUsers();
+
+    }
+    catch (const std::exception& e) {
+        std::cerr << "CONFIG ERROR: " << e.what() << "\n";
+    }
+
+
     return 0;
 }
